39218db56ba14dac008edd1cf3ac7de91f1c0f78
Mazy <mazy_ios@163.com>
2020-09-09 16:02:35 +0800

    add project voice memo

---
+++ Section 2/VoiceMemo/VoiceMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceMemo
+//
+//  Created by Mazy on 2020/9/9.
+//  Copyright © 2020 mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
