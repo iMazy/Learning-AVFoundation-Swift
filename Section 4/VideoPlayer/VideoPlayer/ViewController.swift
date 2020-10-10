55d1aff1a673392b33614e66dee8603063e2912d
Mazy <mazy_ios@163.com>
2020-10-10 16:11:29 +0800

    add section 4 Video player

---
+++ Section 4/VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Mazy on 2020/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
